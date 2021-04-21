c0b9a6893869a6960d9b79ed5ac04faf9615f4b6
Zachary Schneider <hello@zacharyschneider.ca>
2021-04-21 09:27:06 -0700

    📝 formatting, rename class

--- Plugin.php
+++ Plugin.php
@@ -4,7 +4,7 @@ use Storage;
 use Zaxbux\B2\Client;
 use League\Flysystem\Filesystem;
 use Zaxbux\Flysystem\BackblazeB2Adapter;
-use Zaxbux\BackblazeB2StorageDriver\Classes\WinterB2AuthCache;
+use Zaxbux\BackblazeB2StorageDriver\Classes\B2AuthCache;
 
 use System\Classes\PluginBase;
 
@@ -12,13 +12,10 @@ use System\Classes\PluginBase;
  * Plugin class for Backblaze B2 Storage Driver
  */
 class Plugin extends PluginBase {
-	/**
-	 * {@inheritDoc}
-	 */
 	public function boot() {
 		// Register the b2 storage driver
 		Storage::extend('b2', function ($app, $config) {
-			$cache = new WinterB2AuthCache();
+			$cache = new B2AuthCache();
 			$client = new Client($config['applicationKeyId'], $config['applicationKey'], [], $cache);
 			$adaptor = new BackblazeB2Adapter($client, $config['bucketName']);
 			return new Filesystem($adaptor);
